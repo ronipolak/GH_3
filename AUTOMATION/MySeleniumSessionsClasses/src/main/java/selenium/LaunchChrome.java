56ba2dace8d3735a2b2ad8e91481fdd11d03fa0e
ronipolak <ronipolak@gmail.com>
2020-02-22 22:19:24 +0200

    First commit

---
+++ AUTOMATION/MySeleniumSessionsClasses/src/main/java/selenium/LaunchChrome.java
@@ -0,0 +1,18 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","/AUTOMATION/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+
+	}
+
+}
